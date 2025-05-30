bb7dfa1a1bd080ddcdf3a1316b43e0ab81afe97e
panwenbin <panwenbin@gmail.com>
2025-05-30 13:34:31 +0800

    smzdm search only zhi

---
+++ lib/routes/smzdm/search.ts
@@ -0,0 +1,79 @@
+import { Route, ViewType } from '@/types';
+import { load } from 'cheerio';
+import { parseDate } from '@/utils/parse-date';
+import timezone from '@/utils/timezone';
+import puppeteer from '@/utils/puppeteer';
+
+export const route: Route = {
+    path: '/search/:keyword',
+    categories: ['shopping', 'popular'],
+    view: ViewType.Notifications,
+    example: '/smzdm/search/女装',
+    parameters: { keyword: '你想订阅的关键词' },
+    features: {
+        requireConfig: false,
+        requirePuppeteer: true,
+        antiCrawler: true,
+        supportBT: false,
+        supportPodcast: false,
+        supportScihub: false,
+    },
+    name: '关键词',
+    maintainers: ['pwb'],
+    handler,
+};
+
+async function handler(ctx) {
+    const keyword = ctx.req.param('keyword');
+
+    const searchParams = new URLSearchParams({
+        c: 'home',
+        s: keyword,
+        order: 'time',
+        f_c: 'zhi',
+        v: 'a',
+        mx_v: 'a',
+    });
+
+    const browser = await puppeteer({ stealth: true });
+    const page = await browser.newPage();
+
+    await page.goto(`https://search.smzdm.com/?` + searchParams.toString(), {
+        waitUntil: 'networkidle2',
+    });
+
+    const data = await page.content();
+
+    const $ = load(data);
+    const list = $('.feed-row-wide');
+
+    page.close();
+
+    return {
+        title: `${keyword} - 什么值得买`,
+        link: `https://search.smzdm.com/?c=home&s=${encodeURIComponent(keyword)}&order=time`,
+        item:
+            list &&
+            list
+                .toArray()
+                .filter((item) => {
+                    item = $(item);
+                    const zhi = item.find('.z-feed-foot .price-btn-up .unvoted-wrap').eq(0).text().trim();
+                    const unzhi = item.find('.z-feed-foot .price-btn-down .unvoted-wrap').eq(0).text().trim();
+                    const comment = item.find('.z-feed-foot .feed-btn-comment').eq(0).text().trim();
+                    if (comment < 5 || zhi < unzhi * 2) {
+                        return false;
+                    }
+                    return true;
+                })
+                .map((item) => {
+                    item = $(item);
+                    return {
+                        title: `${item.find('.feed-block-title a').eq(0).text().trim()} - ${item.find('.feed-block-title a').eq(1).text().trim()}`,
+                        description: `${item.find('.feed-block-descripe').contents().eq(2).text().trim()}<br>${item.find('.feed-block-extras span').text().trim()}<br><img src="http:${item.find('.z-feed-img img').attr('src')}">`,
+                        pubDate: timezone(parseDate(item.find('.feed-block-extras').contents().eq(0).text().trim(), ['MM-DD HH:mm', 'HH:mm']), +8),
+                        link: item.find('.feed-block-title a').attr('href'),
+                    };
+                }),
+    };
+}
